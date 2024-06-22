59b9253d491cc88de0806a2cb6641f54f589f425
Shreya <shreyap7@illinois.edu>
2024-06-22 00:03:25 -0400

    Add Pair class challenge

---
+++ PairClassChallenge/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Pair {
+  public:
+    int a;
+    int b;
+    int sum() {
+      return a + b;
+    }
+};
+
+int main() {
+  Pair p;
+  p.a = 100;
+  p.b = 200;
+  if (p.a + p.b == p.sum()) {
+    std::cout << "Success!" << std::endl;
+  } else {
+    std::cout << "p.sum() returns " << p.sum() << " instead of " << (p.a + p.b) << std::endl;
+  }
+  return 0;
+}
